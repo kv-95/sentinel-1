69714d75674c0be0a296d0178e642e7a570734a9
vishnu_k <vishnudinesh2011@gmail.com>
2022-03-04 10:50:10 +0530

    updated readme, updated code

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-# Sentinel-1 Data Download Using API
+# Working with Sentinel-1 Imagery
 
 ![Python](https://img.shields.io/badge/python-%2314354C.svg?style=for-the-badge&logo=python&logoColor=white)
 ![Logo](Sentinel-1.jpg)
 
 Satellite images are widely being used in the field of GIS and Remote Sensing these days. ESA and USGS are some of the organizations who provide free access to high resolution satellite data. 
+
+## Downloading Data
 
 There are 2 ways by which we can access satellite data. One is through their website using GUI, the other method is through API calls. First method is easy and simple to use but isn't suited if we are downloading the data on a regular basis. 
 
@@ -23,6 +25,14 @@ _**Functionalities in code:**_
 
 * Download one image per month in a specified time period
 * Download all the images available in the specified time period.
+
+<hr>
+
+## Clip Region of Interest (RoI)
+
+The projection of Sentinel-1 imagery poses some issues while clipping the area of interest. Even though the CRS is defined, it is incomplete in several parts. For example the spheriod is having unknow value. This will create datum conflict while clipping. Hence one possible solution for the problem is to reproject the raster by providing the correct and complete CRS and then clipping. 
+
+The python code given provides a method by which we can reproject the raster and then clip the image using the shapefile of the RoI.
 
 <hr>
 
--- crop_raster.py
+++ crop_raster.py
@@ -1,3 +1,4 @@
+# import libraries
 from osgeo import gdal, osr
 import os
 import fiona
@@ -5,45 +6,42 @@ import rasterio
 from rasterio.mask import mask
 from rasterio.plot import show
 
+# load the data
 def load_data(opt, path=None):
     if opt == 1:
-        img = gdal.Open(path[0]+"/"+os.listdir(path[0])[0])
-        shapefile = fiona.open('./shapefile/marni_plot1.shp')
+        img = gdal.Open(path)
+        shapefile = fiona.open('/path/to/shapefile.shp')
         return img, shapefile
     else:
-        raster = rasterio.open("./output_data/reproject_input.tif")
+        raster = rasterio.open("/path/to/reprojected/raster.tif")
         return raster 
 
+# reproject the data
 def reproject(img):
     srs = osr.SpatialReference()
     srs.ImportFromEPSG(4326)
     new_projection = srs.ExportToWkt()
-    gdal.Warp("./output_data/reproject_input.tif", img, dstSRS=new_projection)
+    gdal.Warp("/path/to/reprojected/raster.tif", img, dstSRS=new_projection)
 
+# clip the raster image
 def clip_raster(shapefile):
     raster = load_data(2)
     shapes = [feature["geometry"] for feature in shapefile]
     
-    out_image, out_transform = mask(raster, shapes, crop=True)
-    out_meta = raster.meta
-    out_meta.update({"driver": "GTiff",
-                 "height": out_image.shape[1],
-                 "width": out_image.shape[2],
-                 "transform": out_transform})
+    crop_image, crop_transform = mask(raster, shapes, crop=True)
+    crop_meta = raster.meta
+    crop_meta.update({"driver": "GTiff",
+                 "height": crop_image.shape[1],
+                 "width": crop_image.shape[2],
+                 "transform": crop_transform})
 
-    with rasterio.open("./output_data/clip_raster.tif", "w", **out_meta) as dest:
-        dest.write(out_image)
-    
-#     display_raster(dest)
+    with rasterio.open("/path/to/clipped/raster.tif", "w", **crop_meta) as dest:
+        dest.write(crop_image)
 
-# def display_raster(output_img):
-#     show(output_img)
-    
-
+# driver code
 if __name__ == '__main__':
 
-    data_dir = sorted(os.listdir("data"))
-    path = list(map(lambda x: "data/"+x+"/measurement", data_dir))
+    path = "/path/to/sentinel-1/imagery.tif"
 
     img, shapefile = load_data(1, path)
 
